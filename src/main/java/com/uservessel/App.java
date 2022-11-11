1ba8eeabbe8c00309329702bbcf661b479fe75b4
Maria Koutsikou <mkoutsikou@scorpiogroup.net>
2022-11-11 10:13:14 +0200

    added UserMenu.java to the code

--- src/main/java/com/uservessel/App.java
+++ src/main/java/com/uservessel/App.java
@@ -1,13 +1,11 @@
 package com.uservessel;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
+ 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        UserMenu.getUserInput();
     }
 }
